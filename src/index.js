e40e10d2a5050d47863488f2ef9aa8a6f3ac3f43
aghayevagha <agayevagha@gmail.com>
2022-12-15 23:02:00 +0400

    done

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./redux/store";
+import { StrictMode } from "react";
+
+ReactDOM.render(
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/pages/ListPage/ListPage.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getPostMovies } from "../../redux/actions/actions";
+import "./ListPage.css";
+
+function ListPage(props) {
+  const [title, setTitle] = useState("");
+  const id = props.match.params.id;
+  const movies = useSelector((state) => state.reducer.postMovies);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    axios
+      .get(`https://acb-api.algoritmika.org/api/movies/list/${id}`)
+      .then((res) => res.data)
+      .then((data) => {
+        setTitle(data.title);
+        data.movies.forEach((imdbID) => dispatch(getPostMovies(imdbID)));
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div className="list-page">
+      <h1 className="list-page__title">{title}</h1>
+      <ul>
+        {movies.map((item) => (
+          <li key={item[0].imdbID}>
+            <a
+              href={"https://www.imdb.com/title/" + item[0].imdbID}
+              className="link__block"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item[0].Title} ({item[0].Year})
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ListPage;
